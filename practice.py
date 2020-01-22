92875a6749a9330f1d06448d602568d0fe06abb5
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-22 11:23:15 +0900

    kana

--- practice.py
+++ practice.py
@@ -1,4 +1,6 @@
-s = sum(list(map(int, input().split())))
-t = sum(list(map(int, input().split())))
-
-print(max(s, t))
+data = {}
+for i in range(int(input())):
+	p,n = input().split();
+	data[p] = data.get(p,0) + int(n)
+for i, j in sorted([[len(a),a] for a in data.keys()]):
+    print (j, data[j])
