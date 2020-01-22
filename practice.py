2e95c84900145e91249a72cdcd10c4d8a916bb5d
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-22 11:21:18 +0900

    test

--- practice.py
+++ practice.py
@@ -1,9 +1,4 @@
-def sec(h,m,s):
-	return 3600*h+60*m+s
+s = sum(list(map(int, input().split())))
+t = sum(list(map(int, input().split())))
 
-def hms(sec):
-	return f"{sec//3600} {(sec//60)%60} {sec%60}"
-
-for i in range(3):
-	h,m,s,H,M,S=map(int,input().split())
-	print(hms(sec(H,M,S)-sec(h,m,s)))
+print(max(s, t))
