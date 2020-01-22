5c1dba72a82ee908ca7eba5eb3e4cf15c65acc22
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-22 12:25:17 +0900

    my first commit

--- practice.py
+++ practice.py
@@ -1,14 +1,11 @@
-def solve():
-    from sys import stdin
-    f_i = stdin
-    
-    n = int(f_i.readline())
-    
-    items = (f_i.readline().split() for i in range(n))
-    items = [(int(v), int(w), t, int(d), n) for v, w, t, d, n in items]
-    items.sort()
-    items = (' '.join(map(str, i)) for i in items)
-    
-    print('\n'.join(items))
+if __name__ == '__main__':
 
-solve()
+	input()
+	A = set(map(int,input().split()))
+	int(input())
+	B = set(map(int,input().split()))
+
+	if B <= A:
+		print("1")
+	else:
+		print("0")
