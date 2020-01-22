61e132b6e302457080cc42a69451e732f84a902f
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-22 11:26:16 +0900

    my second commit

--- practice.py
+++ practice.py
@@ -1,10 +1,2 @@
-import math
-n=int(input())
-a=list(map(int,input().split()))
-a_s=list(set(a))
-while len(a_s)!=1:
-    gcd=math.gcd(a_s[-1],a_s[-2])
-    lcm=int(a_s[-1]*a_s[-2]/gcd)
-    a_s=a_s[:-2]
-    a_s.append(lcm)
-print(a_s[0])
+a,b=map(int,input().split())
+print(a%b)
