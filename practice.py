38ad8870f47f55d8748155388c9b0e5e1c8600f1
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-22 12:10:00 +0900

    my third commit

--- practice.py
+++ practice.py
@@ -1,13 +1,20 @@
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
-p = int(input())
-
-x = a*p
-if c >= p:
-    y = b
-else:
-    y = b + (p-c)*d
-
-print(min([x, y]))
+while True:
+    h,w = map(int,input().split())
+    if(h+w == 0):
+        break
+    li = list()
+    for i in range(h):
+        s = input()
+        li.append(s)
+    d = dict()
+    for i,l in enumerate(li):
+        for j,char in enumerate(l):
+            d[char] = (i,j)
+    target = input()
+    ans = 0
+    now = (0,0)
+    for t in target:
+        next = d[t]
+        ans += abs(now[0]-next[0]) + abs(now[1]-next[1])
+        now=next
+    print(ans + len(target))
