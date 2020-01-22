5130d685a968cc5f89051eb653eddabac5d9da92
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-22 11:42:15 +0900

    my first commit

--- practice.py
+++ practice.py
@@ -1,14 +1,11 @@
-n, k = map(int, input().split())
-ans = []
-for i in range(2**n):
-    cnt = 0
-    for j in range(n):
-        if i>>j&1 ==1:
-            cnt += 1
-    if cnt == k:
-        st = []
-        for t in range(n):
-            if i>>t&1 ==1:
-                st.append(t)
-        print("{}:".format(i),end =" ")
-        print(*st)
+if __name__ == '__main__':
+
+	input()
+	A = set(map(int,input().split()))
+	input()
+	B = set(map(int,input().split()))
+
+	C = sorted(A^B)
+
+	for i in C:
+		print(i)
