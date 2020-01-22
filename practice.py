f7b8145d6a5c2d8e77723252ab4975bc601400c8
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-22 12:27:54 +0900

    my second commit

--- practice.py
+++ practice.py
@@ -1,11 +1,25 @@
-if __name__ == '__main__':
+def solve():
+    from sys import stdin
+    f_i = stdin
+    
+    n, q = map(int, f_i.readline().split())
+    S = [[] for i in range(n)]
+    ans = []
+    
+    for i in range(q):
+        query = f_i.readline()
+        op = query[0]
+        if op == '0':
+            t, x = query[2:].split()
+            S[int(t)].append(x)
+            continue
+        s = S[int(query[2:])]
+        if s:
+            if op == '1':
+                ans.append(s[-1])
+            else:
+                s.pop()
+    
+    print('\n'.join(ans))
 
-	input()
-	A = set(map(int,input().split()))
-	int(input())
-	B = set(map(int,input().split()))
-
-	if B <= A:
-		print("1")
-	else:
-		print("0")
+solve()
