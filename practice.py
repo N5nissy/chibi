3ed5005ed3bbe9bf34654c91e1cc1aaa7f3dc717
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-22 12:21:40 +0900

    my second commit

--- practice.py
+++ practice.py
@@ -1,14 +1,6 @@
 n = int(input())
+num = list(map(int, input().split()))
 
-li = [int(input()) for i in range(n)]
-
-ans = 0
-for a in li:
-    flag = True
-    for i in range(2, int(a**0.5)+1):
-        if a % i == 0:
-            flag = False
-            break
-    if flag:
-        ans += 1
-print(ans)
+ans = list(set(num))
+ans.sort()
+print(' '.join(str(a) for a in ans))
