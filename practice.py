420a3819a3bc0b62ca687a2b30355233a8c1a2b4
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-22 11:48:14 +0900

    commit

--- practice.py
+++ practice.py
@@ -1,9 +1,5 @@
-import itertools
-
-if __name__ == '__main__':
-
-	n = int(input())
-	seq = list(itertools.permutations(range(1,n+1)))
-
-	for p in seq:
-		print(*p)
+while True:
+    try:
+        print(sum(map(int, input().split())))
+    except:
+        break
