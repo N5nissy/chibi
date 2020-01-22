61b6017b9ba9c620273a7ac80dc19eace5f2f182
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-22 11:40:14 +0900

    my third commit

--- practice.py
+++ practice.py
@@ -1,1 +1,14 @@
-print(sum(list(map(int,input().split()))))
+n, k = map(int, input().split())
+ans = []
+for i in range(2**n):
+    cnt = 0
+    for j in range(n):
+        if i>>j&1 ==1:
+            cnt += 1
+    if cnt == k:
+        st = []
+        for t in range(n):
+            if i>>t&1 ==1:
+                st.append(t)
+        print("{}:".format(i),end =" ")
+        print(*st)
