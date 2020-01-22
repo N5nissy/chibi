436415ac8bcc5caf92c9e674a70d6372f7c57f06
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-22 12:29:44 +0900

    my third commit

--- practice.py
+++ practice.py
@@ -1,25 +1,3 @@
-def solve():
-    from sys import stdin
-    f_i = stdin
-    
-    n, q = map(int, f_i.readline().split())
-    S = [[] for i in range(n)]
-    ans = []
-    
-    for i in range(q):
-        query = f_i.readline()
-        op = query[0]
-        if op == '0':
-            t, x = query[2:].split()
-            S[int(t)].append(x)
-            continue
-        s = S[int(query[2:])]
-        if s:
-            if op == '1':
-                ans.append(s[-1])
-            else:
-                s.pop()
-    
-    print('\n'.join(ans))
-
-solve()
+for i in range(1,10) :
+	for j in range(1,10) :
+		print(str(i)+"x"+str(j)+"="+str(i*j))
