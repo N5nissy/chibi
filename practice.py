1d52c3f0a75043a4e32a6ae4e5cd1dac4d848e99
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-22 11:50:06 +0900

    my first commit

--- practice.py
+++ practice.py
@@ -1,5 +1,22 @@
-while True:
-    try:
-        print(sum(map(int, input().split())))
-    except:
-        break
+n = int(input())
+m = int(input())
+card = list(range(1, 2*n+1))
+
+for _ in range(m):
+    ope = int(input())
+    if ope == 0:
+        card1 = card[:n]
+        card2 = card[n:]
+        card = []
+        for c1, c2 in zip(card1, card2):
+            card.append(c1)
+            card.append(c2)
+    else:
+        card1 = card[:ope]
+        card2 = card[ope:]
+        card = []
+        card.extend(card2)
+        card.extend(card1)
+
+for c in card:
+    print(c)
