96709fe2c8a28773061e1a81e41d288641dcc7b4
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-22 12:04:17 +0900

    my second commit

--- practice.py
+++ practice.py
@@ -1,10 +1,13 @@
-n = int(input())
-S = set()
-for i in range(n):
-    query, x = map(int, input().split())
-    if query == 0:
-        S.add(x)
-        print(len(S))
-    else:
-        print(1 if x in S else 0)
-        
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+p = int(input())
+
+x = a*p
+if c >= p:
+    y = b
+else:
+    y = b + (p-c)*d
+
+print(min([x, y]))
