40c6cdbafd226a935f1cf56f9cf098c827af3ffd
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-22 12:01:31 +0900

    my first commit

--- practice.py
+++ practice.py
@@ -1,11 +1,10 @@
-while True:
-    h,w = map(int, input().split())
-    if h == 0 and w == 0: break
-    for j in range(h):
-        for i in range(w):
-            if i == 0 or j == 0 or i == w-1 or j == h-1:
-                print("#", end="")
-            else:
-                print(".", end="")
-        print()
-    print()
+n = int(input())
+S = set()
+for i in range(n):
+    query, x = map(int, input().split())
+    if query == 0:
+        S.add(x)
+        print(len(S))
+    else:
+        print(1 if x in S else 0)
+        
