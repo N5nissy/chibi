136512c95eeebffaf378ece61dad9d8357b26b59
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-22 11:55:44 +0900

    practice

--- practice.py
+++ practice.py
@@ -1,22 +1,11 @@
-n = int(input())
-m = int(input())
-card = list(range(1, 2*n+1))
-
-for _ in range(m):
-    ope = int(input())
-    if ope == 0:
-        card1 = card[:n]
-        card2 = card[n:]
-        card = []
-        for c1, c2 in zip(card1, card2):
-            card.append(c1)
-            card.append(c2)
-    else:
-        card1 = card[:ope]
-        card2 = card[ope:]
-        card = []
-        card.extend(card2)
-        card.extend(card1)
-
-for c in card:
-    print(c)
+while True:
+    h,w = map(int, input().split())
+    if h == 0 and w == 0: break
+    for j in range(h):
+        for i in range(w):
+            if i == 0 or j == 0 or i == w-1 or j == h-1:
+                print("#", end="")
+            else:
+                print(".", end="")
+        print()
+    print()
