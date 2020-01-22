81dbd24840b3670657529493b931ec833784ea74
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-22 11:09:19 +0900

    homework

--- example.py
+++ example.py
@@ -1,5 +1,19 @@
-Scores = [int(input()) for i in range(5)]
-for j in range(len(Scores)):
-    if Scores[j] < 40:
-        Scores[j] = 40
-print(sum(Scores)//5)
+import math
+def main():
+    L = int(input())
+    A = int(input())
+    B = int(input())
+    C = int(input())
+    D = int(input())
+
+    hoge = math.ceil(A / C)
+    fuga = math.ceil(B / D)
+
+    if hoge >= fuga:
+        print(L - hoge)
+    else:
+        print(L - fuga)
+
+
+if __name__ == "__main__":
+    main()
