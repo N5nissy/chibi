22fb067849edd765671c04257a4159f86ef7083a
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-22 11:37:55 +0900

    my second commit

--- practice.py
+++ practice.py
@@ -1,7 +1,1 @@
-n,m=map(int,input().split())
-s=x=0
-for i in range(m):
-    a,b=map(int,input().split())
-    if n-a>0:s+=n-a
-    if n-a>x:x=n-a
-print(s-x)
+print(sum(list(map(int,input().split()))))
