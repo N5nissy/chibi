347590baf0c285aa653e4a2c9003e162f428d3ce
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-22 11:29:03 +0900

    commit

--- practice.py
+++ practice.py
@@ -1,2 +1,13 @@
-a,b=map(int,input().split())
-print(a%b)
+q = int(input())
+
+l = []
+for _ in range(q):
+    a = input().split()
+    if a[0] == "2":
+        l.pop()
+    else:
+        m,q = map(int, a)
+        if m == 0:
+            l.append(q)
+        else:
+            print(l[q])
