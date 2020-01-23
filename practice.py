66bcbf223c2e9e4ae73597f7815492ca56bdaaef
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-23 14:12:59 +0900

    my third commit

--- practice.py
+++ practice.py
@@ -1,23 +1,6 @@
-def selection_sort(A):
-    count = 0
-    for i in range(len(A)):
-        minv = 101
-        minj = -1
-        for j in range(i, len(A)):
-            if minv > A[j]:
-                minv = A[j]
-                minj = j
-        if A[i] != A[minj]:
-            A[i], A[minj] = A[minj], A[i]
-            count += 1
+import math
+a, b, c = map(int, input().split())
 
-    return count
-
-
-n = int(input())
-A = [int(x) for x in input().split()]
-
-count = selection_sort(A)
-
-print(' '.join([str(x) for x in A]))
-print(count)
+print(a*b*math.sin(math.radians(c))/2)
+print(math.sqrt(a**2+b**2-2*a*b*math.cos(math.radians(c)))+a+b)
+print(b*math.sin(math.radians(c)))
