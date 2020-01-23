cd692f51f0c5a17231a1987dc27caee66bd5c829
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-23 16:22:44 +0900

    commit

--- practice.py
+++ practice.py
@@ -1,2 +1,10 @@
-num = list(map(int, input().split()))
-print(min(num), max(num))
+
+n = int(input())
+a = list(map(int, input().split()))
+q = int(input())
+for i in range(q):
+  c, b, e = list(map(int, input().split()))
+  if c==0:
+    print(min(a[b:e]))
+  else:
+    print(max(a[b:e]))
