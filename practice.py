4772eba97e675606e952a67126214dc62156312b
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-23 14:17:54 +0900

    my commit

--- practice.py
+++ practice.py
@@ -1,30 +1,15 @@
-import copy
-def bubble(box,n):
-    for i in range(n):
-        p=n-1
-        while p!=i:
-            if box[p][1]<box[p-1][1]:
-                box[p],box[p-1]=box[p-1],box[p]
-            p -=1
-    return box
+string = input()
 
-def selection(box,n):
-    for i in range(n):
-        mini=i
-        for j in range(i,n):
-            if box[j][1]<box[mini][1]:
-                mini=j
-        if box[i][1]>box[mini][1]:
-            box[i],box[mini]=box[mini],box[i]
-    return box
+n = int(input())
 
-n=int(input())
-A=list(input().split())
-B=copy.deepcopy(A)
-
-bubble=bubble(A,n)
-print(*A)
-print("Stable")
-selection=selection(B,n)
-print(*B)
-print("Stable" if A==B else "Not stable")
+for i in range(n):
+    command = input().split()
+    c_n = command[0]
+    c_i1 = int(command[1])
+    c_i2 = int(command[2]) + 1
+    if c_n == "print":
+        print(string[c_i1:c_i2])
+    elif c_n == "reverse":
+        string = string[:c_i1] + string[c_i1:c_i2][::-1] + string[c_i2:]
+    else:
+        string = string[:c_i1] + command[3] + string[c_i2:]
