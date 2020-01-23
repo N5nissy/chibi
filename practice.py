40e348b821291e0da309b320badfa6e220b84c78
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-23 14:08:12 +0900

    my first commit

--- practice.py
+++ practice.py
@@ -1,5 +1,5 @@
-while True:
-    try:
-        print(int(float(input())**2/98+2))
-    except:
-        break
+d=input().split()
+for _ in range(int(input())):
+ t,f=[d.index(s)for s in input().split()]
+ n=[(1,2,4,3,1),(0,3,5,2,0),(0,1,5,4,0),(1,0,4,5,1),(0,2,5,3,0),(1,3,4,2,1)][t]
+ print(d[n[n.index(f)+1]])
