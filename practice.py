acb1d71b438802ac4eea4022fe44118009d54b18
OBAYASHI Kana <21716025@jwu.ac.jp>
2020-01-23 14:15:13 +0900

    my commit

--- practice.py
+++ practice.py
@@ -1,6 +1,30 @@
-import math
-a, b, c = map(int, input().split())
+import copy
+def bubble(box,n):
+    for i in range(n):
+        p=n-1
+        while p!=i:
+            if box[p][1]<box[p-1][1]:
+                box[p],box[p-1]=box[p-1],box[p]
+            p -=1
+    return box
 
-print(a*b*math.sin(math.radians(c))/2)
-print(math.sqrt(a**2+b**2-2*a*b*math.cos(math.radians(c)))+a+b)
-print(b*math.sin(math.radians(c)))
+def selection(box,n):
+    for i in range(n):
+        mini=i
+        for j in range(i,n):
+            if box[j][1]<box[mini][1]:
+                mini=j
+        if box[i][1]>box[mini][1]:
+            box[i],box[mini]=box[mini],box[i]
+    return box
+
+n=int(input())
+A=list(input().split())
+B=copy.deepcopy(A)
+
+bubble=bubble(A,n)
+print(*A)
+print("Stable")
+selection=selection(B,n)
+print(*B)
+print("Stable" if A==B else "Not stable")
